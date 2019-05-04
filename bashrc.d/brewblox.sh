db09e0d05a588a6f44597d5ca193a26abd325be3
Andy Loftus <aloftus@gmail.com>
2019-05-04 13:28:50 -0500

    Add reset option

--- bashrc.d/brewblox.sh
+++ bashrc.d/brewblox.sh
@@ -2,11 +2,29 @@ bb() {
     action="$1"
     shift
     case "$action" in
-        st) action=status
+        st)
+            action=status
             ;;
-        start) action=up
+        start)
+            action=up
             ;;
-        stop) action=down
+        stop)
+            action=down
+            ;;
+        reset)
+            echo "Are you sure about that?" 1>&2
+            echo "If yes, use 'hard-reset'" 1>&2
+            return
+            ;;
+        hard-reset)
+            # see also:
+            # https://community.brewpi.com/t/brewblox-assistence/3669/86
+            pushd ~/brewblox
+            curl -k -X DELETE https://localhost/spark-one/objects
+            /usr/local/bin/brewblox-ctl down
+            /usr/local/bin/brewblox-ctl setup
+            popd
+            return
             ;;
     esac
     pushd ~/brewblox
