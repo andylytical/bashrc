40cac8023a2ad595dc65958925e32ad3c8ced566
Andy Loftus <aloftus@gmail.com>
2019-05-04 12:59:33 -0500

    Update brewblox.sh

--- bashrc.d/brewblox.sh
+++ bashrc.d/brewblox.sh
@@ -1,1 +1,15 @@
-alias bbc='cd ~/brewblox; brewblox-ctl'
+bb() {
+    action="$1"
+    shift
+    case "$action" in
+        st) action=status
+            ;;
+        start) action=up
+            ;;
+        stop) action=down
+            ;;
+    esac
+    pushd ~/brewblox
+    /usr/local/bin/brewblox-ctl "$action" "${@}"
+    popd
+}
