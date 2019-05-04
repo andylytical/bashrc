384e1a80e8ad5fe1ec5cb408f916b9d8c8928564
Andy Loftus <aloftus@gmail.com>
2019-05-04 07:47:57 -0500

    Exit if no git

--- quickstart.sh
+++ quickstart.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 tmpdir=$(mktemp -d)
-#cd $tmpdir
+GIT=$(which git)
+[[ -z "$GIT" ]] && {
+    echo "ERROR: Cmd 'git' not found. Is 'git' installed?" >&2
+    echo "Exiting." >&2
+    exit 1
+}
 git clone https://github.com/andylytical/bashrc.git $tmpdir/bashrc
 $tmpdir/bashrc/setup.sh
-#cd ~
 rm -rf $tmpdir
