73e5439ef835acbc2eb0d6976c2cb22f9eb473e9
Andy Loftus <aloftus@gmail.com>
2019-08-09 13:30:50 -0500

    Add ~/bin to path.

--- bashrc.d/00_base.sh
+++ bashrc.d/00_base.sh
@@ -3,3 +3,15 @@ set -o vi
 export EDITOR=/usr/bin/vim
 alias ll='ls -l'
 alias la='ls -la'
+
+pathmunge () {
+	if ! echo $PATH | /bin/egrep -q "(^|:)$1($|:)" ; then
+		if [ "$2" = "after" ] ; then
+			PATH=$PATH:$1
+		else
+			PATH=$1:$PATH
+		fi
+	fi
+}
+
+pathmunge ~/bin
