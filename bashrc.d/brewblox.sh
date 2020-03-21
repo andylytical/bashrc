5f6359d3c1866a82dc1a6ade423dd3073f6d1e8c
Andy Loftus <aloftus@gmail.com>
2020-03-21 15:42:50 -0500

    Update brewblox commands

--- bashrc.d/brewblox.sh
+++ bashrc.d/brewblox.sh
@@ -1,33 +1,18 @@
 bb() {
-    action="$1"
+    local _action="${1:-help}"
     shift
-    case "$action" in
-        st)
-            action=status
+    case "$_action" in
+        h|help)
+            _action='--help'
             ;;
         start)
-            action=up
+            _action=up
             ;;
         stop)
-            action=down
-            ;;
-        reset)
-            echo "Are you sure about that?" 1>&2
-            echo "If yes, use 'hard-reset'" 1>&2
-            return
-            ;;
-        hard-reset)
-            # see also:
-            # https://community.brewpi.com/t/brewblox-assistence/3669/86
-            pushd ~/brewblox
-            curl -k -X DELETE https://localhost/spark-one/objects
-            /usr/local/bin/brewblox-ctl down
-            /usr/local/bin/brewblox-ctl setup
-            popd
-            return
+            _action=down
             ;;
     esac
     pushd ~/brewblox
-    /usr/local/bin/brewblox-ctl "$action" "${@}"
+    /usr/local/bin/brewblox-ctl "$_action" "${@}"
     popd
 }
