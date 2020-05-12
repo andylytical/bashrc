c8a59f246294b95cc4f70fb944f80b4729892976
andylytical <aloftus@gmail.com>
2020-05-12 10:58:13 -0500

    Add support for start/stop/restart/logs a single service
    Better "status" output
    
    Can specify a custom linecount for logs cmd:
    ... bb logs SERVICE 80
    ... will yield 80 lines of logs
    ... default linecount is 40

--- bashrc.d/brewblox.sh
+++ bashrc.d/brewblox.sh
@@ -1,23 +1,62 @@
 bb() {
-    local _action="${1:-help}"
+    local _action="${1:-help}"; shift
     local _cmd=/home/pi/.local/bin/brewblox-ctl
+    local _opts=( "${@}" )
     shift
     case "$_action" in
         h|help)
             _action='--help'
             ;;
-        status|stat|s)
+        logs)
+            [[ ${#_opts[@]} -lt 1 ]] && {
+                echo "ERR missing service name"
+                return 1
+            }
+            # allow adjustable linecount at end of cmd, ie:
+            # bb logs SERVICE 80
+            # should yield cmdline:
+            # docker-compose logs -t --tail=80 SERVICE
             _cmd=docker-compose
+            local _linecount=${_opts[1]:-40}
+            local _tmp=( "--tail=$_linecount" "${_opts[0]}" )
+            _opts=( "${_tmp[@]}" )
+            ;;
+        restart)
+            # if additional args, assume it is a single service name to restart
+            [[ ${#_opts[@]} -gt 0 ]] && {
+                _cmd=docker-compose
+            }
+            ;;
+        status|st|s)
+            _cmd=docker
             _action=ps
+            _opts=( '--filter' \
+                    "label=com.docker.compose.project=brewblox" \
+                    '--format' \
+                    "table {{.Names}}\t{{.RunningFor}}\t{{.Status}}" \
+                  )
             ;;
         start)
             _action=up
+            # if additional args, assume it is a single service name to restart
+            [[ ${#_opts[@]} -gt 0 ]] && {
+                _cmd=docker-compose
+                local _tmp=( '-d' "${_opts[@]}" )
+                _opts=( "${_tmp[@]}" )
+            }
             ;;
         stop)
             _action=down
+            # if additional args, assume it is a single service name to restart
+            [[ ${#_opts[@]} -gt 0 ]] && {
+                _cmd=docker-compose
+                _action=stop
+            }
             ;;
     esac
     pushd ~/brewblox
-    "$_cmd" "$_action" "${@}"
+    set -x
+    "$_cmd" "$_action" "${_opts[@]}"
+    set +x
     popd
 }
