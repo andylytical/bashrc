2e7f227715c6e2e4b09ff600c44147f1cc65d5f3
Andy Loftus <aloftus@gmail.com>
2021-07-05 13:20:49 -0500

    add .mintty for "git bash on windows"

--- bashrc.d/10_dircolors.sh
+++ bashrc.d/10_dircolors.sh
@@ -1,5 +1,15 @@
 # Set solarized-dark directory colors
+
+## BASH
+url=https://raw.githubusercontent.com/speedpacer/gitbash_solarized/master/.dir_colors
 fn=~/.dir_colors-solarized-dark
-[[ -f "$fn" ]] \
-|| curl -s -o "$fn" https://raw.githubusercontent.com/speedpacer/gitbash_solarized/master/.dir_colors
+[[ -f "$fn" ]] || curl -s -o "$fn" "$url"
 eval $(dircolors "$fn")
+
+## MINTTY (git bash for windows)
+url=https://raw.githubusercontent.com/speedpacer/gitbash_solarized/master/.minttyrc
+fn=~/.minttyrc
+[[ -f "$fn" ]] || {
+  > "$fn" \
+  curl -s "$url" \
+  | sed '/FontHeight/c\FontHeight=11'
