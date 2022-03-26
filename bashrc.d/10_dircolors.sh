fbabb35fb0dd340ac18cbd77e99b7a86da160dad
Andy Loftus <aloftus@gmail.com>
2022-03-26 13:25:48 -0500

    dircolors checks OS

--- bashrc.d/10_dircolors.sh
+++ bashrc.d/10_dircolors.sh
@@ -1,16 +1,30 @@
 # Set solarized-dark directory colors
 
-## BASH
-url=https://raw.githubusercontent.com/speedpacer/gitbash_solarized/master/.dir_colors
-fn=~/.dir_colors-solarized-dark
-[[ -f "$fn" ]] || curl -s -o "$fn" "$url"
-eval $(dircolors "$fn")
-
-## MINTTY (git bash for windows)
-url=https://raw.githubusercontent.com/speedpacer/gitbash_solarized/master/.minttyrc
-fn=~/.minttyrc
-[[ -f "$fn" ]] || {
-  > "$fn" \
-  curl -s "$url" \
-  | sed '/FontHeight/c\FontHeight=11'
-}
+osname=$(uname -s)
+case "$osname" in
+  Linux)
+    ## BASH
+    url_dir_colors=https://raw.githubusercontent.com/speedpacer/gitbash_solarized/master/.dir_colors
+    fn=~/.dir_colors-solarized-dark
+    [[ -f "$fn" ]] || curl -s -o "$fn" "$url_dir_colors"
+    eval $(dircolors "$fn")
+    ;;
+  MINGW*)
+    ## MINTTY (git bash for windows)
+    fn=~/.minttyrc
+    url_mintty=https://raw.githubusercontent.com/speedpacer/gitbash_solarized/master/.minttyrc
+    marker='#SOLARIZED'
+    grep -q -F "$marker" "$fn" 2>/dev/null || {
+      echo "$marker" >> $fn
+      curl -s "$url_mintty" >> "$fn"
+    }
+    marker='#CUSTOM'
+    grep -q -F "$marker" "$fn" 2>/dev/null || {
+      echo "$marker" >> $fn
+      echo "FontHeight=11" >> "$fn"
+      echo "CursorBlinks=no" >> "$fn"
+      echo "Columns=90" >> "$fn"
+      echo "Rows=50" >> "$fn"
+    }
+    ;;
+esac
