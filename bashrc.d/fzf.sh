64c692146aef56adcef249c78067bffc123e7889
Andy Loftus <aloftus@gmail.com>
2026-07-17 12:53:27 +0000

    fzf.sh

---
+++ bashrc.d/fzf.sh
@@ -0,0 +1,17 @@
+# fzf picker for tmuxp Workspaces
+ft() {
+  local workspace tmuxp_dir ext
+  tmuxp_dir="${HOME}"/.config/tmuxp #full path to your tmuxp config dir
+  ext=.yaml #extension you use for your yaml files
+  workspace=$(find "${tmuxp_dir}" -type f \
+  | xargs -I{} basename {} "${ext}" \
+  | fzf \
+    --header "Pick tmuxp workspace" \
+    --preview "cat ${tmuxp_dir}/{}.yaml")
+
+  if [[ -n "${workspace}" ]]; then
+    tmuxp load "${workspace}"
+  else
+    echo "No workspace given"
+  fi
+}
